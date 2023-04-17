d516187c38c5f56824508ac628450144e88c6b73
AzureAD\RevathyM <revathy.m@williamslea.com>
2023-04-17 20:45:43 +0530

    First commit : Userdata created and intialize first object

--- ObjectToJson/Program.cs
+++ ObjectToJson/Program.cs
@@ -1,7 +1,22 @@
-﻿internal class Program
+﻿using ObjectToJson;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+
+    }
+    static void CreateJsonFile()
+    {
+        UserData user1 = new UserData();
+        Console.WriteLine("Enter userid : ");
+        user1.UserId = Console.ReadLine();
+        Console.WriteLine("Enter First name : ");
+        user1.FirstName = Console.ReadLine();
+        Console.WriteLine("Enter Last name : ");
+        user1.LastName = Console.ReadLine();
+        Console.WriteLine("Enter email : ");
+        user1.Email = Console.ReadLine();
 
     }
 }
